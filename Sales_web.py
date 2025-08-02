5d4262043490fcffb9ac2eb52bcae604e922bea6
Sham kumar.S <shyam0015kumar@gmail.com>
2025-08-02 20:30:28 +0530

    Add files via upload

--- Sales_web.py
+++ Sales_web.py
@@ -2,23 +2,16 @@ import streamlit as st
 import pandas as pd
 
 # Title
-st.title("👋 Welcome to My Simple Website")
+st.title("Nee oru potta da")
 
 # Text
-st.write("This is a basic web app made with Streamlit.")
+#st.write("Nee oru potta da")
 
 # Image from a URL
-st.image("https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png", caption="A Sample Image")
-
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOXmN7u4S-fH_r2FOfBBMjvdtYREs_fxgJRg&s", caption="A Sample Image")
 # Small dataset
-data = {
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [25, 30, 22],
-    "City": ["Delhi", "Mumbai", "Bangalore"]
-}
 
-df = pd.DataFrame(data)
 
-# Show table
-st.write("Here's a small table:")
-st.dataframe(df)
+
+
+
